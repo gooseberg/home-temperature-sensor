9948f0737c5ef1be7abb604252d021b584bce52b
Jordan Henderson <hendersonj219@gmail.com>
2018-11-21 21:50:13 -0800

    Implement basic temperature reading

--- client/src/App.js
+++ client/src/App.js
@@ -13,29 +13,40 @@ class App extends Component {
       }
     }
   }
-  postToggle(data) {
+  postRequest(data) {
     let init = this.postInit(data);
     const URL = '/api';
-    fetch(URL, init).then(resp => console.log(resp));
+    return fetch(URL, init);
   }
   toggleOn = (e) => {
     console.log('toggling on', this, e);
-    this.postToggle({action: 'callFunction', name: 'led', argument: '1'});
+    this.postRequest({action: 'callFunction', name: 'led', argument: '1'});
   }
   toggleOff = (e) => {
     console.log('toggling on', this, e);
-    this.postToggle({action: 'callFunction', name:'led', argument: '0'});
+    this.postRequest({action: 'callFunction', name:'led', argument: '0'});
   }
-  getPin = (e) => {
-    console.log('getting pin', this, e);
-    this.postToggle({action: 'getVariable', name: 'pin'})
+  getTemp = (e) => {
+    console.log('getting temp', this, e);
+    this.postRequest({action: 'getVariable', name: 'temp'})
+      .then((resp) => {
+        resp.json()
+            .then(json => {
+              console.log(json);
+              this.setState({temp: this.convertTemp(json.value)});
+            })
+        });
+  }
+  convertTemp(temp) {
+    return Math.floor(temp/1000);
   }
   render() {
     return (
       <div>
         <button onClick={this.toggleOn}> Toggle On </button>
         <button onClick={this.toggleOff}> Toggle Off </button>
-        <button onClick={this.getPin}>Get Pin</button>
+        <button onClick={this.getTemp}>Get Temp</button>
+        <h1>{this.state ? `${this.state.temp} deg c` : 'No Data'}</h1>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -22,46 +22,64 @@ app.listen(port, () => {
 
 });
 
-// app.post('/login', function (req, res) {
-// 	var userName = req.body.user;
-// 	var password = req.body.password;
-// 	if (userName == 'admin' && password == 'default') {
-// 		console.log("Attempting to log in with default user pass");
-// 		userName = 'robismor@gmail.com';
-// 		password = Buffer.from('ZWxkZWxyb2JzdGVy', 'base64').toString("ascii");
-// 	}
-// 	particle.login({ username: userName, password: password }).then(
-// 		function (data) {
-// 			token = data.body.access_token;
-// 			console.log('Logged in successfully.');
-// 		},
-// 		function (err) {
-// 			console.log('Could not log in.', err.body);
-// 		});
-// });
 app.get('/api', function (req, res) {
 	console.log('recieved get req');
 	res.send('It worked');
 });
 
-app.post('/api', function (req, res) {
-	console.log('recieved post req', req.body);
-	let body = req.body;
-	let token = process.env.PARTICLE_TOKEN;
-	let devicesReq = particle.listDevices({auth: token});
-	devicesReq.then(devices => {
-		console.log(devices.body[0]);
-		let reece = devices.body[0];
-		let id = reece.id;
-		let particleOptions = {auth: token, deviceId: id, name: body.name};
-		if (body.action === 'getVariable') {
-			particle.getVariable(particleOptions).then(resp => console.log(resp));
-		} else if (body.action === 'callFunction') {
-			particleOptions.argument = body.argument;
-			console.log(particleOptions);
-			particle.callFunction(particleOptions).then(resp => console.log(resp), error => console.log(error.body));
-		}
-		res.send('It worked');
+app.post('/api', (req, res) => {
+	console.log('recieved post req2', req.body);
+	let action = req.body.action;
+	postRequest(req, particleRequest).then(resp => {
+		handleParticleResp(resp, action, res);
 	});
-	
 });
+
+const handleParticleResp = (resp, type, res) => {
+	console.log('resp', resp, type);
+	let out = {value: resp.body.result, success: true};
+	res.send(JSON.stringify(out));
+}
+
+const requestLookup = {
+	'getVariable': particle.getVariable.bind(particle),
+	'callFunction': particle.callFunction.bind(particle),
+}
+const rautek_1 = '44002c001847343337363432';
+
+const particleRequest = (body, options) => {
+	let id = rautek_1;
+	let token = process.env.PARTICLE_TOKEN;
+	let func = requestLookup[body.action] || null;
+	let baseOptions = { auth: token, deviceId: id, name: body.name };
+	let finalOptions = Object.assign({}, options, baseOptions);
+	return func(finalOptions);
+};
+
+const postRequest = (req, callback, options = {}) => {
+	let body = req.body;
+	if (body.argument) {
+		options.argument = body.argument;
+	}
+	return callback(body, options);
+}
+// app.post('/api', function (req, res) {
+// 	console.log('recieved post req', req.body);
+// 	let body = req.body;
+// 	let token = process.env.PARTICLE_TOKEN;
+// 	let devicesReq = particle.listDevices({auth: token});
+// 	devicesReq.then(devices => {
+// 		console.log(devices.body[0]);
+// 		let reece = devices.body[0];
+// 		let id = reece.id;
+// 		let particleOptions = {auth: token, deviceId: id, name: body.name};
+// 		if (body.action === 'getVariable') {
+// 			particle.getVariable(particleOptions).then(resp => console.log(resp));
+// 		} else if (body.action === 'callFunction') {
+// 			particleOptions.argument = body.argument;
+// 			console.log(particleOptions);
+// 			particle.callFunction(particleOptions).then(resp => console.log(resp), error => console.log(error.body));
+// 		}
+// 		res.send('It worked');
+// 	});
+// });
