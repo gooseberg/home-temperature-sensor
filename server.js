24a967fbc650a848cf796d5c01a732101c5c3268
Jordan Henderson <hendersonj219@gmail.com>
2018-11-21 22:25:30 -0800

    Remove hardcoded device id

--- server.js
+++ server.js
@@ -45,10 +45,9 @@ const requestLookup = {
 	'getVariable': particle.getVariable.bind(particle),
 	'callFunction': particle.callFunction.bind(particle),
 }
-const rautek_1 = '44002c001847343337363432';
 
 const particleRequest = (body, options) => {
-	let id = rautek_1;
+	let id = process.env.RAUTEK_1;
 	let token = process.env.PARTICLE_TOKEN;
 	let func = requestLookup[body.action] || null;
 	let baseOptions = { auth: token, deviceId: id, name: body.name };
@@ -63,23 +62,3 @@ const postRequest = (req, callback, options = {}) => {
 	}
 	return callback(body, options);
 }
-// app.post('/api', function (req, res) {
-// 	console.log('recieved post req', req.body);
-// 	let body = req.body;
-// 	let token = process.env.PARTICLE_TOKEN;
-// 	let devicesReq = particle.listDevices({auth: token});
-// 	devicesReq.then(devices => {
-// 		console.log(devices.body[0]);
-// 		let reece = devices.body[0];
-// 		let id = reece.id;
-// 		let particleOptions = {auth: token, deviceId: id, name: body.name};
-// 		if (body.action === 'getVariable') {
-// 			particle.getVariable(particleOptions).then(resp => console.log(resp));
-// 		} else if (body.action === 'callFunction') {
-// 			particleOptions.argument = body.argument;
-// 			console.log(particleOptions);
-// 			particle.callFunction(particleOptions).then(resp => console.log(resp), error => console.log(error.body));
-// 		}
-// 		res.send('It worked');
-// 	});
-// });
